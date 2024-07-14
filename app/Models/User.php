a68ce28875c1ee9a8a4e1fea6b5ea9d181c3502e
Samer taha <alrazy1@gmail.com>
2024-07-14 11:16:20 +0300

    added commenting on posts feature

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,4 +72,9 @@ class User extends Authenticatable
     {
         return $this->likes()->where('post_id', $post->id)->exists();
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/posts/partials/categories-box.blade.php
+++ resources/views/posts/partials/categories-box.blade.php
@@ -2,10 +2,7 @@
     <h3 class="text-lg font-semibold text-gray-900 mb-3">Recommended Topics</h3>
     <div class="topics flex flex-wrap justify-start gap-2">
         @foreach ($categories as $category)
-         <x-badge
-         wire:navigate href="{{ route('posts.index',['category'=> $category->slug]) }}" :textColor="$category->text_Color" bgColor="$category->bg_Color">
-            {{$category->title}}
-        </x-badge>
+        <x-posts.category-badge :category="$category" />
         @endforeach
     </div>
 </div>
